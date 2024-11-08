5eedcf590c08dae2abf2e76b75ec17e2ce42c41f
Zhafransss <zafranfahrul13@gmail.com>
2024-11-08 11:25:43 +0700

    Update : Colour Hometes

--- resources/views/client/dashboard/hometest.blade.php
+++ resources/views/client/dashboard/hometest.blade.php
@@ -18,12 +18,12 @@
         </div>
 
         <div class="w-1/2 items-center mr-12 pt-48">
-    <h1 class="text-5xl font-bold text-center whitespace-nowrap  text-blue-700">Test Kesehatan Kulit Anda</h1>
+    <h1 class="text-5xl font-bold text-center whitespace-nowrap  text-[#2e94a4]">Test Kesehatan Kulit Anda</h1>
     <p class="  text-center pt-2 font-light w-full max-w-xl mx-auto ">
         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempora, voluptates, rem maxime unde, omnis veritatis fuga laboriosam aliquid quisquam molestias qui officia eius! Cum recusandae vero odio, est error cupiditate Lorem ipsum dolor, sit amet consectetur adipisicing elit. Temporibus natus minus sint, reiciendis ex, facilis dolore nihil consequatur aspernatur maiores molestiaee, commodi similique! Eveniet.
     </p>
     <div class="flex justify-center pt-6"> <!-- Tambahkan div ini untuk membungkus tombol -->
-        <a href="#" class="bg-blue-700 rounded-md  text-white py-2 px-16 text-sm font-bold">Test Sekarang</a>
+        <a href="#" class="bg-[#2e94a4] rounded-md  text-white py-2 px-16 text-sm font-bold">Test Sekarang</a>
     </div>
 </div>
 
